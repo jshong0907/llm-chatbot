629de582581941dc015beaa4803e8473febd0077
Joon <jshong0907@gmail.com>
2025-02-04 16:34:52 +0900

    change prompt and sample

--- src/inference/processor.py
+++ src/inference/processor.py
@@ -10,7 +10,7 @@ class InferenceProcessor:
     MODEL_NAME = "Bllossom/llama-3.2-Korean-Bllossom-3B"
     MAX_TOKEN = 200
     TEMPERATURE = 0.1
-    DO_SAMPLE = True
+    DO_SAMPLE = False
 
     def __init__(self, rag_processor: Optional[RagProcessor] = None):
         self.model = AutoModelForCausalLM.from_pretrained(
@@ -27,6 +27,10 @@ class InferenceProcessor:
         return {
             "max_new_tokens": self.MAX_TOKEN,
             "do_sample": self.DO_SAMPLE,
+            "temperature": 0.1,  # 응답을 더 확정적으로 생성
+            "repetition_penalty": 1.2,  # 반복을 억제
+            "top_k": 50,  # 높은 확률의 토큰들만 선택
+            "top_p": 0.9,  # 누적 확률 기반 토큰 선택 제어
         }
 
     def chat(self, message: str) -> str:
@@ -37,7 +41,9 @@ class InferenceProcessor:
     def _chat(self, message: str) -> str:
         tokenized_input = self.tokenizer.encode(message, return_tensors="pt").to(self.model.device)
         output = self.model.generate(tokenized_input, **self._generation_args)[0]
-        return self.tokenizer.decode(output, skip_special_tokens=True)
+        result = self.tokenizer.decode(output, skip_special_tokens=True)
+        answer_only = result.replace(message.strip(), "").strip()
+        return answer_only
 
     def _chat_with_rag(self, message: str) -> str:
         if self.rag_processor is None:
--- src/inference/prompts.py
+++ src/inference/prompts.py
@@ -1,15 +1,17 @@
 from langchain_core.prompts import PromptTemplate
 
 template = """
-<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-You are a helpful assistant specializing in answering questions about personal financial transaction data.
-Use three sentences maximum and keep the answer as concise as possible.
+당신은 개인 금융 거래 데이터에 대한 질문에 답변하는 데 특화된 유용한 도우미입니다.
+아래 제공된 거래 내역을 참고하여 사용자의 질문에 답변하세요. 거래 내역에는 날짜, 시간, 금액, 상점 이름과 같은 관련 정보가 포함되어 있습니다.
+가장 최근의 관련 거래 데이터를 한 문장으로만 답변하세요. 추가적인 설명이나 불필요한 정보를 포함하지 마세요.
 
-Context
+거래 내역:
 {context}
-<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+질문:
 {question}
-<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
 """
+
 
 custom_rag_prompt = PromptTemplate.from_template(template)
